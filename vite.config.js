2a05e43c4fb015819af89694ddc79f6116dfb88c
karrim0 <karimhnfy1@gmail.com>
2025-11-21 03:40:36 +0200

    Fix: configure for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/myPortfolio/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist",
